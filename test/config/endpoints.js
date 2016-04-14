3c0c101962ca5ff6ec9df22e6a442d0d9d70cf3f
yjshi <380774061@qq.com>
2016-04-14 16:36:06 +0800

    add new host

--- test/config/endpoints.js
+++ test/config/endpoints.js
@@ -1,6 +1,7 @@
 module.exports = {
     host : {
-        demo: 'https://github.com/aimer1124/SuperTestDemo'
+        demo: 'https://github.com/aimer1124/SuperTestDemo',
+        demo_branches: 'https://github.com/aimer1124/SuperTestDemo/branches/all'
     },
     env: process.env.NODE_ENV || 'demo'
 }
