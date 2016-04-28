83e484d17d78eb63d5098afe09fe3c1cdea977b5
yjshi <380774061@qq.com>
2016-04-28 16:28:31 +0800

    modify branch path

--- test/config/endpoints.js
+++ test/config/endpoints.js
@@ -1,7 +1,7 @@
 module.exports = {
     host : {
         master: 'https://github.com/aimer1124/SuperTestWithGrunt',
-        branch: 'https://github.com/aimer1124/SuperTestWithGrunt/tree/uploadFile'
+        branch: 'https://github.com/aimer1124/SuperTestWithGrunt/tree/differentENV'
     },
     env: process.env.NODE_ENV || 'master'
 };
